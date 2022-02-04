a0e02507a865c28aec051f8a20d6827fbccc4d53
KaterinaKugeneva <kugeneva1@gmail.com>
2022-02-04 15:11:49 +0300

    Initial Commit

---
+++ SwiftUITableView/SwiftUITableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITableViewApp.swift
+//  SwiftUITableView
+//
+//  Created by Ekaterina Kugeneva on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
